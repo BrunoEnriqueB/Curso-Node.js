f57d96730a19a6190edff53de087416b60bdb3fd
BrunoEnriqueB <brunobaronenrique@gmail.com>
2021-12-30 20:24:01 -0300

    n Revert "Roteamento"
    This reverts commit b18e00bc5fccae651afb4a87db2d9c440364e554.

--- 3_core_modules/46_renomear_arquivos/index.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-fs.rename('arquivo.txt', 'arquivo_att.txt', (err) => {
-    if (err){ 
-        console.log(err);
-        return;
-    }
-    console.log("arquivo renomeado!");
-})
