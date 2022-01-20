b45c189b0439796d84009832823013c556a211d0
BrunoEnriqueB <brunobaronenrique@gmail.com>
2022-01-20 14:00:36 -0300

    Excluindo dados e finalizando a seção

--- 13_Mongoose/controllers/ProductsController.js
+++ 13_Mongoose/controllers/ProductsController.js
@@ -2,64 +2,62 @@ const Product = require('../models/Product');
 
 
 module.exports = class ProductController {
-    static showAllProducts = async (req, res) => {
-        const products = await Product.find().lean(); //resgatar os produtos e o lean converte os dados pro handlebars conseguir ler
-        console.log(products);
-        res.render('products/allproducts', {products});
-    }
+  static showAllProducts = async (req, res) => {
+      const products = await Product.find().lean(); //resgatar os produtos e o lean converte os dados pro handlebars conseguir ler
+      res.render('products/allproducts', {products});
+  }
 
-    static create = (req, res) => {
-        res.render('products/create')
-    }
+  static create = (req, res) => {
+      res.render('products/create')
+  }
 
     static createProduct = async (req, res) => {
-        try {
-            const { name, description, price, quantity, image } = req.body;
+      try {
+          const { name, description, price, quantity, image } = req.body;
 
-            const product = new Product({name, description, price, quantity, image});
+          const product = new Product({name, description, price, quantity, image});
 
-            await product.save();
+          await product.save();
 
-            res.redirect('/products');
-        } catch (error) {
-            console.log(error)
-        }
+          res.redirect('/products');
+      } catch (error) {
+          console.log(error)
+      }
     }
 
     static showOneProduct = async (req, res) => {
-        const { id } = req.params;
-        const product = await Product.findById(id).lean()
+      const { id } = req.params;
+      const product = await Product.findById(id).lean()
 
-        res.render('products/product', { product });
+      res.render('products/product', { product });
         
     }
 
     static deleteOneProduct = async (req, res) => {
-        try {
-            const { id } = req.params;
-            await Product.findByIdAndRemove(id);
+      const { id } = req.params;
+      await Product.deleteOne({_id: id});
 
-            res.redirect('/products');
-        } catch (error) {
-            console.log(error);
-        }
+      res.redirect('/products');
+
+
     }
 
     static edit = async (req, res) => {
-        const { id } = req.params;
-        const product = await Product.findOne(id)
+      const { id } = req.params;
+      const product = await Product.findById(id).lean()
 
-        res.render('products/edit', { product });
+      res.render('products/edit', { product });
     }
 
     static editProduct = async (req, res) => {
-        const { id, name, description, price, quantity, image } = req.body;
+      const { id, name, description, price, quantity, image } = req.body;
+      const data = {
+        name, description, price, quantity, image
+      }
 
-        const product = new Product(name, description, price, quantity, image);
+      await Product.updateOne({_id: id}, data);
 
-        await product.updateOne(id);
-
-        res.redirect('/products');
-    }
+      res.redirect('/products');
+  }
 
 }
